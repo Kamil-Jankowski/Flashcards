c42f577dd25dcff81efc0881dd9e253fc9671f0f
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-12-15 14:15:57 +0100

    Stage 4 completed

--- src/flashcards/CardBox.java
+++ src/flashcards/CardBox.java
@@ -3,13 +3,26 @@ package flashcards;
 import java.util.*;
 
 class CardBox {
-    private ArrayList<Card> cards = new ArrayList<>();
+    private HashMap<String, String> cards = new LinkedHashMap<>();
 
     void add(Card card) {
-        cards.add(card);
+        cards.putIfAbsent(card.getName(), card.getDefinition());
     }
 
-    Card getCard(int index) {
-        return cards.get(index);
+    String getCardDefinition(String key) {
+        return cards.get(key);
+    }
+
+    String getCardNameOrDefault(String value, String defaultValue) {
+        for (String name : cards.keySet()) {
+            if (getCardDefinition(name).equals(value)) {
+                return name;
+            }
+        }
+        return defaultValue;
+    }
+
+    Set<String> toSet() {
+        return cards.keySet();
     }
 }
--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -10,26 +10,40 @@ public class Main {
         String term;
         String definition;
         String answer;
+        String correctName;
 
         System.out.println("Input the number of cards:");
         int numberOfCards = scanner.nextInt();
         scanner.nextLine();                     // move scanner so it will not skip first input we want to read
 
+        // Putting cards into CardBox
         for (int i = 0; i < numberOfCards; i++){
             System.out.println(String.format("The card #%d", i+1));
             term = scanner.nextLine();
+            while (quenya.toSet().contains(term)){
+                System.out.printf("The card \"%s\" already exists. Try again:\n", term);
+                term = scanner.nextLine();
+            }
             System.out.printf("The definition of the card #%d\n", i+1);
             definition = scanner.nextLine();
+            while (quenya.getCardNameOrDefault(definition, null) != null){
+                System.out.printf("The definition \"%s\" already exists. Try again:\n", definition);
+                definition = scanner.nextLine();
+            }
             cardMaker.createCard(term, definition);
         }
 
-        for (int i = 0; i < numberOfCards; i++){
-            term = quenya.getCard(i).getName();
-            definition = quenya.getCard(i).getDefinition();
-            System.out.printf("Print the definition of \"%s\":\n", term);
+        // Asking about the definition of cards from the CardBox
+        for (String cardName : quenya.toSet()){
+            definition = quenya.getCardDefinition(cardName);
+            System.out.printf("Print the definition of \"%s\":\n", cardName);
             answer = scanner.nextLine();
-            if(definition.equals(answer)){
-                System.out.println("Correct answer.\n");
+            correctName = quenya.getCardNameOrDefault(answer, null);
+            if(cardName.equals(correctName)){
+                System.out.println("Correct answer.");
+            } else if(correctName != null){
+                System.out.printf("Wrong answer. The correct one is \"%s\"", definition);
+                System.out.printf(", you've just written the definition of \"%s\".\n", correctName);
             } else {
                 System.out.printf("Wrong answer. The correct one is \"%s\".\n", definition);
             }
